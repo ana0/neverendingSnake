11cf32251a310e181248a0a8e3db7d37a3d178be
anon <sarah.anne.friend@gmail.com>
2018-01-10 18:16:18 -0500

    Can eat and replace food

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,6 +5,16 @@ const snake = [
   { body: { x: 10, y: 20 },
   move: 'L' },
   { body: { x: 20, y: 20 },
+  move: 'L' },
+  { body: { x: 30, y: 20 },
+  move: 'L' },
+  { body: { x: 40, y: 20 },
+  move: 'L' },
+  { body: { x: 50, y: 20 },
+  move: 'L' },
+  { body: { x: 60, y: 20 },
+  move: 'L' },
+  { body: { x: 70, y: 20 },
   move: 'L' }]
 let pendingMove = 'L';
 
@@ -13,18 +23,31 @@ const maxX = (window.innerWidth - window.innerWidth%10);
 const minY = 0;
 const maxY = (window.innerHeight - window.innerHeight%10);
 
-// const initBoard = (board) => {
-//   const width = window.innerWidth;
-//   const height = window.innerHeight;
-//   board = new Array((width - width%10)/10);
-//   board = board.map((item) => new Array((height - height%10)/10));
-// }
+const foodLocation = {};
+
+const placeFood = () => {
+	const randomX = Math.random();
+	foodLocation.x = (maxX * randomX) - (maxX * randomX)%10;
+	const randomY = Math.random();
+	foodLocation.y = (maxY * randomY) - (maxY * randomY)%10;
+}
+
+const drawFood = () => {
+	rect(foodLocation.x, foodLocation.y, 10, 10);
+}
 
 const drawSnake = (snake) => {
   snake.map((segment) => {
   	console.log('drawing snake segment')
     rect(segment.body.x, segment.body.y, 10, 10)
   })
+}
+
+const detectCollision = () => {
+  if (snake[0].body.x === foodLocation.x && snake[0].body.y === foodLocation.y) {
+  	snake.push({ body: { x: snake[snake.length-1].body.x, y: snake[snake.length-1].body.y }, move: snake[snake.length-1].move })
+  	placeFood();
+  }
 }
 
 const prepareNextMove = (snake) => {
@@ -36,6 +59,9 @@ const prepareNextMove = (snake) => {
 
 const moveSnake = (snake) => {
   snake = snake.map((segment, index, array) => {
+  	if (index === array.length-1) {
+  		detectCollision()
+  	}
   	switch(segment.move) {
   	  case 'R':
   	    if (segment.body.x < maxX) segment.body.x = segment.body.x + 10;
@@ -75,16 +101,15 @@ function keyPressed() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // initGame();
+  placeFood()
 }
 
 function draw() {
   background(255)
   fill(0)
+  drawFood();
   drawSnake(snake);
   moveSnake(snake);
   prepareNextMove(snake);
   //ellipse(50, 50, 80, 80);
 }
-
-console.log('loaded')
