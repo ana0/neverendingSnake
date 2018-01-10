277f5fac073a764dbabdaee17527e4596ba4cc00
anon <sarah.anne.friend@gmail.com>
2018-01-10 08:54:54 -0500

    Snake data structure and grid

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,18 +1,53 @@
+const board = [];
+const snake = [
+  { body: { x: 0, y: 20 },
+  move: false }, 
+  { body: { x: 10, y: 20 },
+  move: false },
+  { body: { x: 1360, y: 650 },
+  move: false }]
 
-const initBoard = () => {
+const minX = 0;
+const maxX = (window.innerWidth - window.innerWidth%10);
+const minY = 0;
+const maxY = (window.innerHeight - window.innerHeight%10);
+
+const initBoard = (board) => {
   const width = window.innerWidth;
   const height = window.innerHeight;
-  const board = new Array(height/10)
+  board = new Array((width - width%10)/10);
+  board = board.map((item) => new Array((height - height%10)/10));
 }
+
+const drawSnake = (snake) => {
+  snake.map((segment) => {
+  	console.log('drawing snake segment')
+    rect(segment.body.x, segment.body.y, 10, 10)
+  })
+}
+
+// directions
+// R = x + 10
+// L = x - 10
+// U = y - 10
+// D = y + 10
 
 const initGame = () => {
-
+  initBoard(board);
+  console.log(board);
 }
 
-const setup = () => {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  initGame();
 }
 
-const draw = () => {
-  // box();
-  ellipse(50, 50, 10, 10);
+function draw() {
+  fill(0)
+  drawSnake(snake);
+  console.log(window.innerWidth)
+  console.log(window.innerHeight)
+  //ellipse(50, 50, 80, 80);
 }
+
+console.log('loaded')
